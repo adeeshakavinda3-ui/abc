2253e2d961e62751c9682f10d1ba052bbc966fff
adeeshakavinda3-ui <adeeshakavinda3@gmail.com>
2025-09-18 16:42:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=181050ff8257eccf' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'https://github.com/nbbb15092/abc' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
